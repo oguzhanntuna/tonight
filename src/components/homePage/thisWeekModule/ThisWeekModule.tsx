a15f949be06a9409dc587b805d3b056f5555280d
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-11-11 02:44:29 +0300

    thisWeekModule event redesigned and tab updated

--- src/components/homePage/thisWeekModule/ThisWeekModule.tsx
+++ src/components/homePage/thisWeekModule/ThisWeekModule.tsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from 'react';
 import './ThisWeekModule.scss';
 
 import image from '../../../assets/Screen Shot 2021-11-09 at 4.19.30 PM.png';
+import moreIcon from '../../../assets/icons/arrow-forward-outline.svg';
 
 const ThisWeekModule = (): JSX.Element => {
     const daysOfTheWeekArray: Array<string> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Firday', 'Saturday', 'Sunday'];
+    const [activeDaysFilter, setActiveDaysFilter] = useState<number>(0);
+
+    // useEffect(() => {
+
+    // }, []);
 
     const renderDaysOfTheWeek = (): JSX.Element => {
-        
+       
         return (
             <div className="daysOfTheWeekFilters">
                 {
                     daysOfTheWeekArray.map((day, index) => (
-                        <div className="daysOfTheWeekFilters-days" key={`${day}-${index}`}>
+                        <div 
+                            className={`daysOfTheWeekFilters-days ${activeDaysFilter === index ? 'active' : ''}`} 
+                            onClick={() => setActiveDaysFilter(index)}
+                            key={`${day}-${index}`}
+                        >
                             {day}
                         </div>
                     ))
@@ -24,83 +35,18 @@ const ThisWeekModule = (): JSX.Element => {
         
         return (
             <div className="eventsThisWeek">
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
+                <div className="eventsThisWeek-eventContainer">
+                    <div className="eventsThisWeek-event">
+                        <div className="eventsThisWeek-image" style={{ backgroundImage: `url("${image}")` }} />
+                        <div className="eventsThisWeek-imageOverlay" />
+                        <div className="eventsThisWeek-content">
+                            <div className="eventsThisWeek-location">Klein Phönix</div>
+                            <div className="eventsThisWeek-date">10 Nov 2021 Wed 18:00 - 00:00</div>
+                            
+                            <div className="eventsThisWeek-title">Enrico Sangiuliano</div>
                         </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
                     </div>
-                </div>
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
-                        </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
-                    </div>
-                </div>
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
-                        </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
-                    </div>
-                </div>
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
-                        </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
-                    </div>
-                </div>
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
-                        </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
-                    </div>
-                </div>
-                <div className="eventsThisWeek-event">
-                    <div className="eventsThisWeek-imageContainer">
-                        <img className="eventsThisWeek-image" src={image} alt="" />
-                        <div className="eventsThisWeek-title">Title</div>
-                    </div>
-                    <div className="eventsThisWeek-infoContainer">
-                        <div className="eventsThisWeek-info">
-                            <div className="eventsThisWeek-date">Sunday</div>
-                            <div className="eventsThisWeek-location">Klein</div>
-                        </div>
-                        <button className="eventsThisWeek-buyNowButton">Buy Now</button>
-                    </div>
+                    <button className="eventsThisWeek-purchaseButton">Buy Now</button>
                 </div>
             </div>
         );
@@ -112,7 +58,15 @@ const ThisWeekModule = (): JSX.Element => {
                 <div className="thisWeekModule-title">
                     This Week
                 </div>
-                { renderDaysOfTheWeek() }
+                { 
+                    renderDaysOfTheWeek() 
+                }
+                <div className="thisWeekModule-moreButton">
+                    <div className="thisWeekModule-text">See All</div>
+                    <div className="thisWeekModule-moreIcom">
+                        <img src={moreIcon} alt="moreIcom" />
+                    </div>
+                </div>
             </div>
             { renderEvents() }
         </div>   
