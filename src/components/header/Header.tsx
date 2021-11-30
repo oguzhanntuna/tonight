0320c9c8c3aef75133321e1f98d4a7a7e738eecc
oguzhanntuna <oguzhana.tuna@gmail.com>
2021-12-01 00:17:44 +0300

    blur and box-shadow added to the header based on scroll position

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,26 @@
 import { Link, NavLink } from 'react-router-dom';
 import'./Header.scss';
 
-const Header = (): JSX.Element => (
-    <div className="header">
-        <Link to="/" className="logo">Tonight</Link>
-        {/* <div className="seperator" /> */}
-        <ul className="navigationContainer">
-            <NavLink to="/events" className="navigationContainer-tab">Events</NavLink>
-        </ul>
-        <div className="userActionContainer">
-            <button className="userActionContainer-login">Log In</button>
-            <button className="userActionContainer-signup">Sign Up</button>
+const Header = (): JSX.Element => {
+
+    document.addEventListener('scroll', () => {
+        const headerElement = document.getElementById('header');
+        
+        window.pageYOffset > 0 ? headerElement?.classList.add('blur') : headerElement?.classList.remove('blur');
+    });
+
+    return (
+        <div className="header" id="header">
+            <Link to="/" className="logo">Tonight</Link>
+            <ul className="navigationContainer">
+                <NavLink to="/events" className="navigationContainer-tab">Events</NavLink>
+            </ul>
+            <div className="userActionContainer">
+                <button className="userActionContainer-login">Log In</button>
+                <button className="userActionContainer-signup">Sign Up</button>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Header;
