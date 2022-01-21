767fb5ce0ed9687a112f4d368576332058e9ee97
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-01-21 11:12:55 -0700

    rendered eventTickets in buyNowEventsPage

--- src/pages/buyNowEvents/buyNowEvents.tsx
+++ src/pages/buyNowEvents/buyNowEvents.tsx
@@ -1,15 +1,45 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import './buyNowEvents.scss';
+
+import { IApplicationState } from '../../models/interfaces/store/states/application';
+import * as EventActions from '../../store/actions/events';
 
 import HeroImage from '../../components/heroImage/HeroImage';
 import heroImage from '../../assets/heroImage.jpg';
+import EventTicket from '../../components/eventTicket/EventTicket';
 
 const BuyNowEventsPage = (): JSX.Element => {
+    const { fetchBuyNowEvents } = EventActions;
+    const buyNowEvents = useSelector((state: IApplicationState) => state.events.buyNowEvents);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        if (buyNowEvents && buyNowEvents.length === 0) {
+            dispatch(fetchBuyNowEvents());
+        }
+    }, [buyNowEvents, dispatch, fetchBuyNowEvents]);
 
     return (
         <div className="buyNowEventsPage">
             <HeroImage imageUrl={heroImage} />
             <div className="buyNowEventsPage-content">
-                Buy Now Events Page
+                <div className="buyNowEventsContainer">
+                    <div className="buyNowEventsContainer-title">
+                        Buy Now Events
+                    </div>
+                    <div className="buyNowEventsContainer-events">
+                        { 
+                            buyNowEvents.map((event, index) => (
+                                <EventTicket 
+                                    key={`${index}-${event.id}`} 
+                                    eventData={event} 
+                                />
+                            )) 
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     );
