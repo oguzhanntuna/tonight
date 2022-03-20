8fcccffe44515cf93dcc0225cd647f0ef3fef6af
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-03-19 23:53:05 -0700

    purchased tickets page redesigned and minize order functionality added

--- src/pages/purchasedTickets/purchasedTickets.tsx
+++ src/pages/purchasedTickets/purchasedTickets.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './purchasedTickets.scss';
 
 import { useScrollToTop } from '../../customHooks/useScrollToTop';
 import { IApplicationState } from '../../models/interfaces/store/states/application';
 
+import moreIcon from '../../assets/icons/more.svg';
 import heroImage from '../../assets/hero.jpg';
 import ticketIcon from '../../assets/icons/ticket/ticket-full.svg';
 import HeroImage from '../../components/heroImage/HeroImage';
@@ -12,32 +14,58 @@ import EmptyState from '../../components/emptyState/EmptyState';
 
 const PurchasedTicketsPage = (): JSX.Element => {
     const orders = useSelector((state: IApplicationState) => state.orders.orders);
+    const [isMinimizedOrderIndexArray, setIsMinimizedOrderIndexArray] = useState<Array<number>>([]);
 
     useScrollToTop();
+
+    const minimizeOrder = (orderIndex: number) => {
+        if (isMinimizedOrderIndexArray.includes(orderIndex)) {
+            const newIsMinimizedOrderIndexArray = isMinimizedOrderIndexArray.filter(index => index !== orderIndex);
+
+            setIsMinimizedOrderIndexArray(newIsMinimizedOrderIndexArray);
+        } else {
+            setIsMinimizedOrderIndexArray(isMinimizedOrderIndexArray => [...isMinimizedOrderIndexArray, orderIndex]);
+        }
+    }
 
     const renderPurchasedTickets = () => (
         <div className="orders">
             {
-                orders.map(order => {
+                orders.map((order, orderIndex) => {
                     const purchasedTickets = order.purchasedTickets;
 
                     return (
-                        <div className="orders-orderContainer">
-                            <div className="orders-orderDate">
-                                31 Ocak 2022
+                        <div className={`
+                            orders-orderContainer 
+                            ${isMinimizedOrderIndexArray.includes(orderIndex) ? 'minimized' : ''} 
+                        `}>
+                            <div className="orders-header">
+                                <p className="orders-orderDate">
+                                    31 Ocak 2022
+                                </p>
+                                <div 
+                                    className='orders-minimizeOrderButton'
+                                    onClick={() => minimizeOrder(orderIndex)}
+                                >
+                                    <img src={moreIcon} alt="more" />
+                                </div>
                             </div>
-                            <div className="orders-purchasedTicketsContainer">
-                                { 
-                                    purchasedTickets.map(purchasedTicket => (
-                                        <div className="orders-purchasedTicket">
-                                            {
-                                                // purchasedTicket.title 
-                                                <EventTicket eventData={purchasedTicket} />
+                            {
+                                isMinimizedOrderIndexArray.includes(orderIndex)
+                                    ? <></>
+                                    : (
+                                        <div className="orders-purchasedTicketsContainer">
+                                            <div className="orders-purchasedTicketsContainerOverlay" />
+                                            { 
+                                                purchasedTickets.map(purchasedTicket => (
+                                                    <div className="orders-purchasedTicket">
+                                                        <EventTicket eventData={purchasedTicket} />
+                                                    </div>
+                                                ))
                                             }
                                         </div>
-                                    ))
-                                }
-                            </div>
+                                    )
+                            }
                         </div>
                     )
                 })
