dde557838fee14caf5066357ecd4298745e7664e
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-03-21 21:37:23 -0700

    footer padding and page min height fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,12 @@
+import { useLocation } from 'react-router-dom';
 import './Footer.scss';
 
 const Footer = (): JSX.Element => {
+    const location = useLocation();
+    const { pathname } = location;
 
     return (
-        <div className="footer">
+        <div className={`footer ${pathname === '/cart' ? 'footer-morePaddingForCartPage' : ''}`}>
             <div className="footer-seperator"></div>
             <div className="footer-content">
                 <span>Developed by © Oguzhan Tuna. All Rights Reserved.</span>
